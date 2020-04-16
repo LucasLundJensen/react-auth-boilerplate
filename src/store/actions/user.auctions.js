be733eebc2e92130bc344b668af28034ce900716
Lucas Lund Jensen <lucas.lund@live.dk>
2020-04-16 22:25:55 +0200

    created all actions created a reducer for the actions

--- src/store/actions/user.auctions.js
+++ src/store/actions/user.auctions.js
@@ -1,1 +1,66 @@
+import axios from 'axios';
 import { userConstants } from '../constants/';
+
+export const userActions = {
+    login,
+    logout,
+    failure,
+    success,
+    request
+}
+
+
+function request() {
+    return {
+        type: userConstants.LOGIN_REQUEST
+    };
+};
+
+function login(email, password) {
+    return async dispatch => {
+        try {
+            // Dispatch that the authentication request has been sent
+            dispatch(request());
+
+            const res = await axios.post('http://localhost:3001/api/auth/login', { email: email, password: password });
+
+            // Dispatch that the authentication request was successfull.
+            dispatch(success(res.data.token, res.data.userId));
+
+            // Set the local storage items
+            localStorage.setItem("JWT_TOKEN", res.data.token);
+            localStorage.setItem("USER_ID", res.data.userId);
+
+            // Not sure if this is needed, will have to do some testing.
+            axios.defaults.headers.common["Authorization"] = res.data.token;
+
+        } catch(err) {
+            // If some fails, dispatch the failure.
+            dispatch(failure(err.response.data.message));
+        }
+    }
+}
+
+function logout() {
+    localStorage.removeItem("JWT_TOKEN");
+    localStorage.removeItem("USER_ID");
+
+    return {
+        type: userConstants.LOGOUT
+    }
+}
+
+function failure(error) {
+    return {
+        type: userConstants.LOGIN_FAILURE,
+        error: error
+    }
+}
+
+function success(token, userId) {
+    return {
+        type: userConstants.LOGIN_SUCCESS,
+        token: token,
+        userId: userId
+    }
+}
---
+++ src/store/reducers/authentication.reducer.js
@@ -0,0 +1,53 @@
+import { userConstants } from '../constants';
+import { updateObject } from '../helpers';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+};
+
+function request(state, action) {
+    return updateObject(state, {error: null, loading: true});
+}
+
+function success(state, action) {
+    return updateObject(state, {
+        token: action.token,
+        userId: action.token,
+        loading: false,
+        error: null
+    })
+}
+
+function failure(state, action) {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    })
+}
+
+function logout(state, action) {
+    return updateObject(state, {
+        token: null,
+        userId: null
+    })
+}
+
+const authenticationReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case userConstants.LOGIN_REQUEST:
+            return request(state, action);
+        case userConstants.LOGIN_FAILURE:
+            return failure(state, action);
+        case userConstants.LOGIN_SUCCESS:
+            return success(state, action);
+        case userConstants.LOGOUT:
+            return logout(state, action);
+        default:
+            return state;
+    }
+}
+
+export default authenticationReducer;
